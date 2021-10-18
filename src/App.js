a6c6eaff31608930bcfb27c5990aa050e160e05f
bohyeon <shannon_@hotmail.co.kr>
2021-10-18 23:18:00 +0900

    🎉 begin project!

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const App = () => {
+  return (
+    <div class="helloDiv">
+      <p>Hello, World! ㅎㅇ</p>
+      <p>Hello, React!</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require("path");
+const webpack = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  // 개발환경 mode : "development|production" 개발 환경과 배포 환경을 정할 수 있음
+  mode: "development",
+
+  // entry: 모듈의 의존성이 시작되는 부분. 빌드 작업을 시작할 부분을 명시한다.
+  entry: "./src/index.js",
+
+  //entry에서부터 시작하여 번들링된 파일을 어디에 저장할지(path), 어떤 파일이름으로 저장할지(filename) 명시
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+
+  //module : 어떤 모듈을 사용할지 명시한다.  test 는 어떤 파일에 적용될지 그 확장자를 명시한 것이다.
+  //로더가 1개라면 loader 로, 2개 이상이라면 use 배열로 설정할 수 있다.
+  // exclude 로 로더를 제외할 대상을 적용할 수도 있다.
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: "/node_modules/",
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+      {
+        test: /\.jfif$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+        },
+      },
+      // html관련 plugin
+      //minimize: true를 통해 html 코드를 최적화
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  //plugins : 확장 기능을 넣을 수 있다.
+  // DefinePlugin : 노드 환경정보인 process.env.NODE_ENV에 값을 넣어준다. webpack 설정파일의 mode값을 return함
+  // CleanWebpackPlugin : 빌드 이전 결과물을 제거(output 경로에 있는거)
+  // BannerPlugin : 결과물에 빌드 정보나 커밋 버전, 만든이 등과 같은 정보를 추가할 수 있다
+  // HtmlWebpackPlugin : 번들이 완료된 파일을 <script/>를 이용해 로드한 html파일을 자동으로 생성해주는 플러그인
+  plugins: [
+    new webpack.DefinePlugin({}),
+    new webpack.BannerPlugin({
+      banner: `build time : ${new Date().toLocaleTimeString()}`,
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  // webpack-dev-server의 개발 서버 설정
+  devServer: {
+    host: "localhost",
+    port: 9000,
+    open: true, //개발 서버 실행 시 브라우저 오픈
+  },
+};
