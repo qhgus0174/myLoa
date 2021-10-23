6d447dc20fa8498a0058972c8caf85d1927ee81b
bohyeon <shannon_@hotmail.co.kr>
2021-10-23 15:29:54 +0900

    ✨ toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
 import * as React from 'react';
 import AppRouter from './Router';
+import { Flip, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-    return <AppRouter />;
+    return (
+        <>
+            <ToastContainer
+                position="top-right"
+                autoClose={4000}
+                hideProgressBar
+                newestOnTop
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable={false}
+                pauseOnHover={false}
+                transition={Flip}
+                theme="dark"
+                limit={5}
+            />
+            <AppRouter />
+        </>
+    );
 };
 
 export default App;
