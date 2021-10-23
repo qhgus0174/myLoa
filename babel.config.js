61e88124598f83be8548adc45260219693dc1f10
bohyeon <shannon_@hotmail.co.kr>
2021-10-23 15:29:36 +0900

    ➕ 종속성 추가
    - localstorage 패키지 변경
    - emotion css
    - toast

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    presets: ['@babel/preset-typescript', '@babel/preset-env', '@babel/preset-react'],
+    presets: ['@babel/preset-typescript', '@babel/preset-env', '@babel/preset-react', '@emotion/babel-preset-css-prop'],
     plugins: ['@babel/plugin-transform-runtime', '@emotion/babel-plugin'],
 };
