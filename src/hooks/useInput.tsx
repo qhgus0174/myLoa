1cba7ce91a2a7cb894b7939cb0e644790cdf54c2
bohyeon <shannon_@hotmail.co.kr>
2021-10-23 15:30:14 +0900

    ✨ 커스텀 훅 추가
    - useInput

---
+++ src/hooks/useInput.tsx
@@ -0,0 +1,34 @@
+import React, { useCallback, useState } from 'react';
+
+/**
+ * ? useage : <TextBox {...bindContent} />
+ */
+
+export function useInput<T>(
+    initialValue: T,
+    option?: {
+        numberOnly?: boolean;
+        maxLength?: number;
+    },
+): [T, any, () => void] {
+    const [value, setValue] = useState<T>(initialValue);
+
+    const reset = useCallback(() => setValue(initialValue), [initialValue]);
+
+    const bind = {
+        value,
+        onKeyPress: (e: React.KeyboardEvent<HTMLInputElement>) => {
+            if (option?.numberOnly) {
+                if (!/[0-9]/.test(e.key)) {
+                    e.preventDefault();
+                }
+            }
+        },
+        onChange: (e: React.ChangeEvent<any>) => {
+            setValue(e.target?.value);
+        },
+        maxLength: option?.maxLength,
+    };
+
+    return [value, bind, reset];
+}
