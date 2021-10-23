03cfe391296d777bfd835eb3f12243f9370d771f
bohyeon <shannon_@hotmail.co.kr>
2021-10-23 15:32:04 +0900

    ✨ 캐릭터 추가 모달창 추가

---
+++ src/components/Add/Character.tsx
@@ -0,0 +1,72 @@
+import React, { useContext } from 'react';
+import LabelText from '@components/Input/LabelText';
+import Button from '@components/Button/Button';
+import useLocalStorageState from 'use-local-storage-state';
+import { useInput } from '@hooks/useInput';
+import useCharacter from '@hooks/storage/useCharacter';
+import useCharacterOrd from '@hooks/storage/useCharacterOrd';
+import { ModalActionContext } from '@context/ModalContext';
+import { toast } from 'react-toastify';
+
+export interface ICharacter {
+    id: number;
+    name: string;
+    level: number;
+    job: string;
+    lastSearch: number;
+}
+
+const Character = () => {
+    const [character, setCharacter] = useCharacter();
+    const [characterOrd, setCharacterOrd] = useCharacterOrd();
+
+    const { closeModal } = useContext(ModalActionContext);
+
+    const [name, bindName] = useInput<string>('');
+
+    const validate = () => {
+        const characterArr: ICharacter[] = JSON.parse(character);
+        return characterArr.some(character => character.name === name);
+    };
+
+    const onClickAdd = () => {
+        if (validate()) {
+            toast.error('중복된 캐릭터명 입니다.');
+            return;
+        }
+
+        const characterArr: ICharacter[] = JSON.parse(character);
+        const characterOrdArr: number[] = JSON.parse(characterOrd);
+
+        const maxValueId = Math.max(...characterArr.map(o => o.id), 0);
+
+        const characterId = characterArr.length == 0 ? 0 : maxValueId + 1;
+
+        const characterInfo: ICharacter = {
+            id: characterId,
+            name: name,
+            level: 1325,
+            job: '아르카나',
+            lastSearch: 0,
+        };
+        characterArr.push(characterInfo);
+        characterOrdArr.push(characterId);
+
+        setCharacter(JSON.stringify(characterArr));
+        setCharacterOrd(JSON.stringify(characterOrdArr));
+
+        closeModal();
+    };
+
+    return (
+        <>
+            <div>
+                <LabelText {...bindName} label="캐릭터 명" />
+                <Button onClick={onClickAdd}>추가</Button>
+                <Button>닫기</Button>
+            </div>
+        </>
+    );
+};
+
+export default Character;
