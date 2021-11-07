274801cce05a0174505a63e177ed353d5b6c8140
bohyeon <shannon_@hotmail.co.kr>
2021-11-08 04:38:28 +0900

    💄 반응형 크기 수정

--- src/style/device.ts
+++ src/style/device.ts
@@ -6,11 +6,29 @@ export const customMediaQueryHeight = (maxWidth: number): string => {
     return `@media (max-height: ${maxWidth}px)`;
 };
 
+interface IDevice {
+    desktop: number;
+    smallDesktop: number;
+    tablet: number;
+    phone: number;
+    smallPhone: number;
+}
+
+export const responsiveWidth: IDevice = {
+    desktop: 1280,
+    smallDesktop: 960,
+    tablet: 768,
+    phone: 576,
+    smallPhone: 450,
+};
+
 export const widthMedia = {
     custom: customMediaQueryWidth,
-    desktop: customMediaQueryWidth(1280),
-    tablet: customMediaQueryWidth(768),
-    phone: customMediaQueryWidth(576),
+    desktop: customMediaQueryWidth(responsiveWidth.desktop),
+    smallDesktop: customMediaQueryWidth(responsiveWidth.smallDesktop),
+    tablet: customMediaQueryWidth(responsiveWidth.tablet),
+    phone: customMediaQueryWidth(responsiveWidth.phone),
+    smallPhone: customMediaQueryWidth(responsiveWidth.smallPhone),
 };
 
 export const heightMedia = {
