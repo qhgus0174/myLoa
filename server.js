30f10833b04a371e4994122cb48b7e4a579502b3
bohyeon <shannon_@hotmail.co.kr>
2021-11-09 03:11:03 +0900

    ✏️ bundle path 수정

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
+const distPath = path.join(__dirname, 'dist');
 
-app.use(express.static(publicPath));
+app.use(express.static(distPath));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+    res.sendFile(path.join(distPath, 'index.html'));
 });
 
 app.listen(port, () => {
