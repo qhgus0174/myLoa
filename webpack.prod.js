3cc7de1a8e093365fa551da4e7e651292778a196
bohyeon <shannon_@hotmail.co.kr>
2021-11-09 02:09:11 +0900

    🎨 webpack prod config 주석 제거

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,16 +4,13 @@ const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
-
-    //TerserWebpackPlugin : 자바스크립트 코드를 난독화하고 debugger 구문을 제거한다.
-    //SplitChunksPlugin : 코드를 분리할때 중복을 없앤다.
     optimization: {
         minimize: true,
         minimizer: [
             new TerserPlugin({
                 terserOptions: {
                     compress: {
-                        drop_console: true, // 콘솔 로그를 제거한다
+                        drop_console: true,
                     },
                 },
             }),
