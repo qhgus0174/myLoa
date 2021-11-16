c7def91692c8b5663fcab8b4b28fbe131577d815
bohyeon <shannon_@hotmail.co.kr>
2021-11-16 18:05:47 +0900

    ✅ heroku timezone 세팅 후 cron 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -85,7 +85,7 @@ const App = () => {
     };
 
     useEffect(() => {
-        const jobDaily = new CronJob('0 35 17 * * *', () => resetDailyTodoRelax(), null, false, 'Asia/Seoul');
+        const jobDaily = new CronJob('0 10 18 * * *', () => resetDailyTodoRelax(), null, false, 'Asia/Seoul');
         jobDaily.start();
 
         const jobWeek = new CronJob('0 0 6 * * 3', () => resetWeeklyTodo(), null, false, 'Asia/Seoul');
