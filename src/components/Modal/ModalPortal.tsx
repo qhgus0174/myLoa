9b9541d9556e6b621d18a8149ce0930acdb21d01
bohyeon <shannon_@hotmail.co.kr>
2021-11-16 13:47:00 +0900

    🎨 modal 동작 방식 변경

--- src/components/Modal/ModalPortal.tsx
+++ src/components/Modal/ModalPortal.tsx
@@ -5,8 +5,6 @@ import BasicModal from '@components/Modal';
 import { IPortalOption } from '@common/types';
 import styled from '@emotion/styled';
 import { widthMedia, heightMedia } from '@style/device';
-import { Dimmer } from '@style/common/modal';
-import OutsideAlerter from '@hooks/useOutsideAlerter';
 
 const ModalPortal = () => {
     const { isOpen, content, options } = useContext(ModalStateContext);
@@ -14,42 +12,41 @@ const ModalPortal = () => {
 
     return createPortal(
         isOpen && (
-            <Dimmer tabIndex={-1}>
-                <OutsideAlerter excuteFn={closeModal}>
-                    <ModalInner
-                        tabIndex={0}
-                        width={options?.width}
-                        height={options?.height}
-                        onClick={e => {
-                            e.stopPropagation();
-                        }}
-                    >
-                        <BasicModal options={options}>{content}</BasicModal>
-                    </ModalInner>
-                </OutsideAlerter>
-            </Dimmer>
+            <ModalContainer>
+                <Dimmer tabIndex={-1} onClick={closeModal}></Dimmer>
+                <BasicModal options={options}>{content}</BasicModal>
+            </ModalContainer>
         ),
         document.getElementById('modal-root') as HTMLElement,
     );
 };
+const Dimmer = styled.div`
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.6);
+    backdrop-filter: blur(4px);
+`;
 
-const ModalInner = styled.div<Pick<IPortalOption, 'width' | 'height'>>`
-    padding-left: 2.2rem;
-    padding-right: 2.2rem;
-    padding-bottom: 1.5rem;
+const ModalContainer = styled.div`
+    display: flex;
+    position: fixed;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
     box-sizing: content-box;
     border-radius: 4px;
-    background-color: ${props => props.theme.colors.main};
-    box-shadow: 5px 10px 10px 1px rgba(0, 0, 0, 0.3);
     outline: none;
 
-    overflow: hidden; //각을 없앴을 때 내부 영역이 튀어나오는걸 방지
-
-    width: ${props => (props.width ? props.width : '65')}px;
-    height: ${props => (props.height ? props.height : '60')}px;
-    max-height: 90vh;
-    overflow-y: auto;
-    max-width: 95vw;
+    z-index: 8888;
 `;
 
 export default ModalPortal;
--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,16 +1,44 @@
 import React from 'react';
-import { IPortalProperty } from '@common/types';
+import { IPortalOption, IPortalProperty } from '@common/types';
 import styled from '@emotion/styled';
 import { widthMedia } from '@style/device';
+import { css } from '@emotion/react';
+import { FlexDiv } from '@style/common';
 
 const BasicModal = ({ children, options }: IPortalProperty) => {
     return (
-        <>
+        <ModalInner direction="column" width={options?.width} height={options?.height}>
             {options?.headerTitle && <ModalHeader height={options?.height || '1'}>{options.headerTitle}</ModalHeader>}
             <ModalContent isHeader={options?.headerTitle || ''}>{children}</ModalContent>
-        </>
+        </ModalInner>
     );
 };
+
+const ModalInner = styled(FlexDiv)<Pick<IPortalOption, 'width' | 'height'>>`
+    z-index: 2;
+
+    position: relative;
+    padding-left: 3rem;
+    padding-right: 3rem;
+    padding-top: 1rem;
+    padding-bottom: 2rem;
+    box-sizing: content-box;
+    border-radius: 4px;
+    border: 1px solid ${props => props.theme.colors.mainInner};
+    background-color: ${props => props.theme.colors.main};
+    box-shadow: 5px 10px 10px 1px rgba(0, 0, 0, 0.3);
+
+    overflow-x: hidden;
+    overflow-y: auto;
+
+    color: #c3c0c0;
+
+    width: ${props => (props.width ? props.width : '65')}px;
+    height: ${props => (props.height ? props.height : '60')}px;
+    max-height: 90vh;
+    overflow-y: auto;
+    max-width: 95vw;
+`;
 
 const ModalHeader = styled.div<{ height: string }>`
     display: flex;
