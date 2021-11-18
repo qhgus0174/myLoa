496122a4c385df89003ae615c4183e2d57b2d7e1
bohyeon <shannon_@hotmail.co.kr>
2021-11-18 20:18:10 +0900

    ✨ firebase 환경 변수 설정

--- src/types/custom.d.ts
+++ src/types/custom.d.ts
@@ -27,3 +27,11 @@ declare module '*.png' {
     const src: string;
     export default src;
 }
+
+declare const FIREBASE_API_KEY: string;
+declare const FIREBASE_AUTH_DOMAIN: string;
+declare const FIREBASE_PROJECT_ID: string;
+declare const FIREBASE_STORAGE_BUCKET: string;
+declare const FIREBASE_MESSAGING_SENDER_ID: string;
+declare const FIREBASE_APP_ID: string;
+declare const FIREBASE_MEASUREMENT_ID: string;
