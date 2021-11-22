f0516984e3dfdae73a2d145328ee45abdaf38fec
bohyeon <shannon_@hotmail.co.kr>
2021-11-23 00:30:03 +0900

    ✨ interface db type 추가
    - 필요없는 테이블, 파일 삭제(주간 가디언, 주간 어비스 - 파일로 관리함)

--- src/type/response.ts
+++ src/type/response.ts
@@ -1,5 +1,37 @@
-export interface IResponse {
+export interface IResponse<T = any> {
     status: 'SUCCESS' | 'ERR';
-    result: any;
+    result: T;
     message?: string;
+}
+
+export interface IWeeklyGuardian {
+    id: string;
+    name: string;
+}
+
+export interface IWeeklyAbyss {
+    id: string;
+    name1: string;
+    name2: string;
+}
+
+export interface IWeeklyContents {
+    guardian: string;
+    abyss: string;
+}
+
+export interface IBackup {
+    backupKey: string;
+    todo: string;
+    todoOrd: string;
+    character: string;
+    characterOrd: string;
+}
+
+export interface IError {
+    message: string;
+    dataColumn1?: string;
+    dataColumn2?: string;
+    dataColumn3?: string;
+    errType: 'backup' | 'croll' | 'other';
 }
