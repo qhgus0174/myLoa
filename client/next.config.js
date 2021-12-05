0900c8ef467196694d97fb8adbc9f6e13b99798d
bohyeon <shannon_@hotmail.co.kr>
2021-12-05 12:52:53 +0900

    🔧 nextjs dev server proxy config

--- client/next.config.js
+++ client/next.config.js
@@ -6,6 +6,16 @@ module.exports = withImages({
     images: {
         disableStaticImages: true,
     },
+    async rewrites() {
+        return (
+            isDevelopment && [
+                {
+                    source: '/api/:path*',
+                    destination: 'http://localhost:8080/:path*',
+                },
+            ]
+        );
+    },
     webpack: (config, options) => {
         return {
             ...config,
