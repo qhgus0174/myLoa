7eda5012acc7f3138c6c99db4f2a2772471a31b9
bohyeon <shannon_@hotmail.co.kr>
2021-12-13 04:05:31 +0900

    ✨ 가계부 타입 추가

---
+++ src/components/Ledger/LedgerType.ts
@@ -0,0 +1,41 @@
+export interface ILedger {
+    common: ILedgerCommon;
+    own: ILedgerOwn[];
+}
+
+export interface ILedgerOwn {
+    characterId: number;
+    prevWeekGold: number[];
+    histories: {
+        goods: {
+            fold: boolean;
+            data: ILedgerHistoryGoods[];
+        };
+        raid: {
+            fold: boolean;
+            data: ILedgerHistoryRaid[];
+        };
+    };
+}
+
+export interface ILedgerCommon {
+    prevWeekGold: number[];
+    histories: ICommonHistory[];
+}
+
+export interface ICommonHistory extends ILedgerHistoryRaid {
+    name: string;
+    imgUrl: string;
+}
+
+export interface ILedgerHistoryRaid {
+    id: string;
+    gold: number;
+    datetime: string;
+}
+
+export interface ILedgerHistoryGoods extends ILedgerHistoryRaid {
+    categoryId: string;
+    imgId: string;
+    name: string;
+}
