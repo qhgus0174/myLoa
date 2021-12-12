c790613aceebdbfd0c6a27e257973f5fd7068b69
bohyeon <shannon_@hotmail.co.kr>
2021-12-13 04:06:41 +0900

    ✨ 통계 인터페이스 타입 추가

---
+++ src/components/Statistics/StatisticsType.ts
@@ -0,0 +1,15 @@
+export interface IStatisticsCommon {
+    weekKor: string;
+    gold: number;
+}
+
+export interface IStatisticsPersonal {
+    name: string;
+    raid: number;
+    goods: number;
+}
+
+export interface IStatisticsPersonalPrev {
+    name: string;
+    gold: number;
+}
