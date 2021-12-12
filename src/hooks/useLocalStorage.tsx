67f900d109c4872bb438c7ef8e9a030e231c30da
bohyeon <shannon_@hotmail.co.kr>
2021-12-13 04:11:42 +0900

    🎨 localstorage hook 변경
    - 자꾸 초기화 되서 손봄

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,69 +1,118 @@
 import React, { useEffect, useState } from 'react';
 import { ICharacter } from '@components/Character/CharacterType';
 import { ITodo } from '@components/Todo/TodoType';
+import { ICommonHistory, ILedger, ILedgerOwn } from '@components/Ledger/LedgerType';
+import { DateTime } from 'luxon';
+import { parseStorageItem, stringifyStorageItem } from '@common/utils';
+
+export const initCommonHistory: ICommonHistory[] = [
+    {
+        id: '1',
+        gold: 0,
+        datetime: DateTime.now().toFormat('X'),
+        name: '에포나 증표 교환',
+        imgUrl: '/static/img/lostark/common/eponagold.png',
+    },
+];
+
+export const initLedger: ILedger = {
+    common: {
+        prevWeekGold: [0, 0, 0, 0],
+        histories: initCommonHistory,
+    },
+    own: [],
+};
 
 export const useLocalStorage = () => {
     const [storedTodo, setStoredTodo] = useState<ITodo[]>([]);
     const [storedTodoOrd, setStoredTodoOrd] = useState<number[]>([]);
     const [storedCharacter, setStoredCharacter] = useState<ICharacter[]>([]);
     const [storedCharacterOrd, setStoredCharacterOrd] = useState<number[]>([]);
+    const [storedLedger, setStoredLedger] = useState<ILedger>(initLedger);
 
     useEffect(() => {
-        const todoData = JSON.parse(JSON.parse(localStorage.getItem('todo') as string));
-
-        if (todoData) {
-            setStoredTodo(todoData);
-        }
+        const todoData: ITodo[] = parseStorageItem(localStorage.getItem('todo') as string);
+        todoData && todoData.length > 0 ? setStoredTodo(todoData) : setStoredTodo([]);
     }, []);
 
     useEffect(() => {
-        const todoOrdData = JSON.parse(JSON.parse(localStorage.getItem('todoOrd') as string));
-
-        if (todoOrdData) {
-            setStoredTodoOrd(todoOrdData);
-        }
+        const todoOrdData: number[] = parseStorageItem(localStorage.getItem('todoOrd') as string);
+        todoOrdData && todoOrdData.length > 0 ? setStoredTodoOrd(todoOrdData) : setStoredTodoOrd([]);
     }, []);
 
     useEffect(() => {
-        const characterData = JSON.parse(JSON.parse(localStorage.getItem('character') as string));
-
-        if (characterData) {
-            setStoredCharacter(characterData);
-        }
+        const characterData: ICharacter[] = parseStorageItem(localStorage.getItem('character') as string);
+        characterData && characterData.length > 0 ? setStoredCharacter(characterData) : setStoredCharacter([]);
     }, []);
 
     useEffect(() => {
-        const characterOrdData = JSON.parse(JSON.parse(localStorage.getItem('characterOrd') as string));
-
-        if (characterOrdData) {
-            setStoredCharacterOrd(characterOrdData);
-        }
+        const characterOrdData: number[] = parseStorageItem(localStorage.getItem('characterOrd') as string);
+        characterOrdData && characterOrdData.length > 0
+            ? setStoredCharacterOrd(characterOrdData)
+            : setStoredCharacterOrd([]);
     }, []);
 
     useEffect(() => {
-        localStorage.setItem('todo', JSON.stringify(JSON.stringify(storedTodo)));
+        const storedcharacter: ICharacter[] = parseStorageItem(localStorage.getItem('character') as string);
+        const ledgerData: ILedger = parseStorageItem(localStorage.getItem('ledger') as string);
+        ledgerData ? setStoredLedger(ledgerData) : ledgerInit(storedcharacter);
+    }, []);
+
+    useEffect(() => {
+        storedTodo && storedTodo.length > 0 && localStorage.setItem('todo', stringifyStorageItem(storedTodo));
     }, [storedTodo]);
 
     useEffect(() => {
-        localStorage.setItem('todoOrd', JSON.stringify(JSON.stringify(storedTodoOrd)));
+        storedTodoOrd &&
+            storedTodoOrd.length > 0 &&
+            localStorage.setItem('todoOrd', stringifyStorageItem(storedTodoOrd));
     }, [storedTodoOrd]);
 
     useEffect(() => {
-        localStorage.setItem('character', JSON.stringify(JSON.stringify(storedCharacter)));
+        storedCharacter &&
+            storedCharacter.length > 0 &&
+            localStorage.setItem('character', stringifyStorageItem(storedCharacter));
     }, [storedCharacter]);
 
     useEffect(() => {
-        localStorage.setItem('characterOrd', JSON.stringify(JSON.stringify(storedCharacterOrd)));
+        storedCharacterOrd &&
+            storedCharacterOrd.length > 0 &&
+            localStorage.setItem('characterOrd', stringifyStorageItem(storedCharacterOrd));
     }, [storedCharacterOrd]);
+
+    useEffect(() => {
+        storedLedger &&
+            initLedger != storedLedger &&
+            localStorage.setItem('ledger', stringifyStorageItem(storedLedger));
+    }, [storedLedger]);
+
+    const ledgerInit = (characters: ICharacter[]) => {
+        if (!characters || characters.length < 1) return;
+        console.log('asdvb');
+
+        const commonLedger = initLedger.common;
+
+        const goodsLedger: ILedger['own'] = characters.map((character: ICharacter) => {
+            const charactersLedger: ILedgerOwn = {
+                characterId: character.id,
+                prevWeekGold: [0, 0, 0, 0],
+                histories: { raid: { fold: true, data: [] }, goods: { fold: true, data: [] } },
+            };
+            return charactersLedger;
+        });
+        setStoredLedger(Object.assign({}, { common: commonLedger }, { own: goodsLedger }));
+    };
 
     return {
         storedTodo,
         storedTodoOrd,
         storedCharacter,
         storedCharacterOrd,
+        storedLedger,
         setStoredTodo,
         setStoredTodoOrd,
         setStoredCharacter,
         setStoredCharacterOrd,
+        setStoredLedger,
     };
 };
