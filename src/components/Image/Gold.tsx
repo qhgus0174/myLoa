42f0efd63afe3c28cf13485a188f663040202c91
bohyeon <shannon_@hotmail.co.kr>
2021-12-14 07:16:10 +0900

    🎨 골드 아이콘 파라미터 추가
    - 넓이, 높이

--- src/components/Image/Gold.tsx
+++ src/components/Image/Gold.tsx
@@ -3,10 +3,17 @@ import Image from 'next/image';
 
 interface IImage {
     type: 'basic' | 'stack';
+    width?: string;
+    height?: string;
 }
 
-const GoldIcon = ({ type }: IImage) => (
-    <Image layout="fixed" src={`/static/img/lostark/gold/${type}.png`} width="19" height="15" />
+const GoldIcon = ({ type, width, height }: IImage) => (
+    <Image
+        layout="fixed"
+        src={`/static/img/lostark/gold/${type}.png`}
+        width={width ? width : '19'}
+        height={height ? height : '15'}
+    />
 );
 
 export default GoldIcon;
