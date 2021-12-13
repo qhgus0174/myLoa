c3e6c25618178e4e944cfc256e8837d2a13faf21
bohyeon <shannon_@hotmail.co.kr>
2021-12-14 07:21:46 +0900

    🗑️ 필요 없는 코드 삭제
    - select box로 하려고 했는데 안 씀

--- src/apis/ledger/common.ts
+++ src/apis/ledger/common.ts
@@ -2,8 +2,6 @@ import { IResponse } from '@common/types/response';
 import { ICommonGold } from '@common/types/response/ledger/common';
 import axios from 'axios';
 
-export const initCommonDropDown: ICommonGold = { id: '', name: '수입 경로를 선택 해 주세요.' };
-
 export const getCommon = async (): Promise<ICommonGold[]> => {
     const { result: commonGoldResult } = (await (
         await axios.get(`/api/ledger/commonGold`)
