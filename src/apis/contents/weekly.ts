5c38c5fb3c63c4ff12e82c0143037806f2e402b4
bohyeon <shannon_@hotmail.co.kr>
2021-12-19 03:29:25 +0900

    🎨 ssr 위한 base url 추가

--- src/apis/contents/weekly.ts
+++ src/apis/contents/weekly.ts
@@ -3,6 +3,8 @@ import { IResponse } from '@common/types/response';
 import { IWeeklyContents } from '@common/types/response/contents/weekly';
 
 export const getWeekContents = async (): Promise<IWeeklyContents> => {
-    const { result } = (await (await axios.get(`/api/weeklyContents`)).data) as IResponse<IWeeklyContents>;
+    const { result } = (await (
+        await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/weeklyContents`)
+    ).data) as IResponse<IWeeklyContents>;
     return result;
 };
