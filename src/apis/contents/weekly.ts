f221933f5060ede79e3895585c3820fea162a081
bohyeon <shannon_@hotmail.co.kr>
2021-12-19 09:53:28 +0900

    ✅ api url test

--- src/apis/contents/weekly.ts
+++ src/apis/contents/weekly.ts
@@ -4,7 +4,7 @@ import { IWeeklyContents } from '@common/types/response/contents/weekly';
 
 export const getWeekContents = async (): Promise<IWeeklyContents> => {
     const { result } = (await (
-        await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/weeklyContents`)
+        await axios.get(`https://myloa.kro.kr/api/weeklyContents`)
     ).data) as IResponse<IWeeklyContents>;
     return result;
 };
