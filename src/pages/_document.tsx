3062890a034b75fd47147b28f09a6fbfb0b305b2
bohyeon <shannon_@hotmail.co.kr>
2021-12-19 18:56:00 +0900

    🎨 tag manager 설정 변경

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,17 @@ export default class CustomDocument extends Document {
         return (
             <Html>
                 <Head>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PLC2ZF2');
+              `,
+                        }}
+                    />
                     <title>로요일좋아</title>
                     <meta
                         name="description"
@@ -27,6 +38,14 @@ export default class CustomDocument extends Document {
                     <meta property="og:url" content="https://loa-day.com/" />
                 </Head>
                 <body>
+                    <noscript>
+                        <iframe
+                            src="https://www.googletagmanager.com/ns.html?id=GTM-PLC2ZF2"
+                            height="0"
+                            width="0"
+                            style={{ display: 'none', visibility: 'hidden' }}
+                        ></iframe>
+                    </noscript>
                     <Main />
                     <div id="modal-root"></div>
                 </body>
