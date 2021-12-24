c87d45fbca79901ec39c79dcdbf3ebd2745c472a
bohyeon <shannon_@hotmail.co.kr>
2021-12-25 05:28:56 +0900

    ⏪ theme 렌더링 제거 다시 Rollback
    - localStorage 사용 하지 않고 로그인 하는 방식 고려, SSR 적용이 어려움..

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -2,7 +2,7 @@ import { IThemeStyle } from '@style/theme';
 import React, { useEffect, useState } from 'react';
 
 export const useTheme = () => {
-    const [theme, setTheme] = useState<IThemeStyle>('basic');
+    const [theme, setTheme] = useState<IThemeStyle>('');
 
     useEffect(() => {
         const themeData = localStorage.getItem('theme') as IThemeStyle;
