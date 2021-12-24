774072f78cf2aa4f146572a40f0355cd5daa4d81
bohyeon <shannon_@hotmail.co.kr>
2021-12-25 04:45:32 +0900

    ✅ theme 렌더링 적용 제거

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -2,7 +2,7 @@ import { IThemeStyle } from '@style/theme';
 import React, { useEffect, useState } from 'react';
 
 export const useTheme = () => {
-    const [theme, setTheme] = useState<IThemeStyle>('');
+    const [theme, setTheme] = useState<IThemeStyle>('basic');
 
     useEffect(() => {
         const themeData = localStorage.getItem('theme') as IThemeStyle;
