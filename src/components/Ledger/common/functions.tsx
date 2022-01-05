7cdb9285d328fbdccddadf3f70182d08be19c28e
bohyeon <shannon_@hotmail.co.kr>
2022-01-05 20:09:15 +0900

    🎨 sum parameter type 변경 (공통으로 사용 위함)

--- src/components/Ledger/common/functions.tsx
+++ src/components/Ledger/common/functions.tsx
@@ -1,6 +1,6 @@
 import { ILedgerHistoryRaid } from '@common/types/localStorage/Ledger';
 
-export const calcSum = (arr: ILedgerHistoryRaid[]): number => {
+export const calcSum = (arr: any[]): number => {
     return arr.reduce((prev, { gold }) => prev + gold, 0);
 };
 
