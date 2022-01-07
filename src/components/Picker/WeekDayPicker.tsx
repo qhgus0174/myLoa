5497003c22ff6809da8c6556bd541cad850dcbc4
bohyeon <shannon_@hotmail.co.kr>
2022-01-07 22:46:48 +0900

    ✨ 로요일 주 선택 radio 컴포넌트 추가

---
+++ src/components/Picker/WeekDayPicker.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { getThisWeek } from '@common/utils';
+import styled from '@emotion/styled';
+import RadioButton from '@components/Input/Radio';
+import { DateTime } from 'luxon';
+
+interface IWeekDayPicker {
+    day: string;
+    setDay: (e: string) => void;
+}
+
+const WeekDayPicker = ({ day, setDay }: IWeekDayPicker) => {
+    return (
+        <Containter>
+            {getThisWeek().map((goldDay: string, index) => {
+                return (
+                    <RadioButton
+                        key={index}
+                        text={DateTime.fromFormat(goldDay, 'yyyy/MM/dd').toFormat('MM/dd')}
+                        name="contents"
+                        value={goldDay}
+                        onChange={e => {
+                            setDay(e.target.value);
+                        }}
+                        checked={day === goldDay}
+                    />
+                );
+            })}
+        </Containter>
+    );
+};
+
+const Containter = styled.section`
+    display: flex;
+`;
+
+export default WeekDayPicker;
