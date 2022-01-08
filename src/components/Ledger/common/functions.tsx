8bbf5a7b6688f5a323b457a2d4b2b240cb73d46d
bohyeon <shannon_@hotmail.co.kr>
2022-01-08 16:35:30 +0900

    ✨ 공통 골드 계산 함수 추가

--- src/components/Ledger/common/functions.tsx
+++ src/components/Ledger/common/functions.tsx
@@ -1,4 +1,4 @@
-import { ILedgerHistoryRaid } from '@common/types/localStorage/Ledger';
+import { ICommonHistory, ILedgerHistoryRaid } from '@common/types/localStorage/Ledger';
 
 export const calcSum = (arr: any[]): number => {
     return arr.reduce((prev, { gold }) => prev + gold, 0);
@@ -6,4 +6,20 @@ export const calcSum = (arr: any[]): number => {
 
 export const calcSumWithCommma = (arr: ILedgerHistoryRaid[]): string => {
     return arr.reduce((prev, { gold }) => prev + gold, 0).toLocaleString();
+};
+
+export const calcCommonIncomeGold = ({ history }: { history: ICommonHistory[] }) => {
+    const result = history.filter(({ type }) => {
+        return type != 'spending';
+    });
+
+    return result;
+};
+
+export const calcCommonSpendingGold = ({ history }: { history: ICommonHistory[] }) => {
+    const result = history.filter(({ type }) => {
+        return type == 'spending';
+    });
+
+    return result;
 };
