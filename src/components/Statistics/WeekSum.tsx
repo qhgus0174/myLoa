f25ab256aadab7cc079b29030a68c1206d2593ed
bohyeon <shannon_@hotmail.co.kr>
2022-01-08 16:37:36 +0900

    🎨 골드 합 컴포넌트 로직 수정

--- src/components/Statistics/WeekSum.tsx
+++ src/components/Statistics/WeekSum.tsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { FlexDiv } from '@style/common';
+import { FlexDiv } from '@style/common/layout/common';
 import TitleAndGold from '@components/Ledger/TitleAndGold';
 import { widthMedia } from '@style/device';
+import { useTheme } from '@emotion/react';
+import { IncomeSpendingType } from '@common/types/types';
 
 interface IWeekSum {
     common: number;
     personal: number;
+    type: IncomeSpendingType | 'all';
 }
 
-const WeekSum = ({ common, personal }: IWeekSum) => {
+const WeekSum = ({ common, personal, type }: IWeekSum) => {
+    const theme = useTheme();
+
+    const textColor =
+        type === 'all' ? theme.colors.text : type === 'income' ? theme.ledger.income : theme.ledger.spending;
+
+    const titleCommon = type === 'all' ? '공통 수입' : type === 'income' ? `공통 수입` : `공통 지출`;
+    const titlePersonal = type === 'all' ? '캐릭터 별 수입' : type === 'income' ? `캐릭별 수입` : `캐릭별 지출`;
+
+    const isNagative = type === 'all' ? false : type === 'income' ? false : true;
+
     return (
         <Container direction="column">
             <GoldList>
-                <TitleAndGold title="공통 합" gold={common} />
-                <TitleAndGold title="개인별 합" gold={personal} />
-                <TitleAndGold className="total" title="총 계" gold={common + personal} />
+                <TitleAndGold goldTextColor={textColor} title={titleCommon} gold={common} negative={isNagative} />
+                <TitleAndGold goldTextColor={textColor} title={titlePersonal} gold={personal} negative={isNagative} />
+                <TitleAndGold goldTextColor={textColor} title="총 계" gold={common + personal} negative={isNagative} />
             </GoldList>
         </Container>
     );
